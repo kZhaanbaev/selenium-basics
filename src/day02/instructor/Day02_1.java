6439acbde83a707024fe0dc93084b0ed457771b6
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-08-18 22:06:23 -0400

    day2 end

--- src/day02/instructor/Day02_1.java
+++ src/day02/instructor/Day02_1.java
@@ -1,4 +1,26 @@
 package day02.instructor;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Day02_1 {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        //holds program until n number of milliSeconds
+        Thread.sleep(3000);
+
+        //get title of the page
+        System.out.println(driver.getTitle());
+
+        System.out.println(driver.getCurrentUrl());
+
+        driver.close();
+    }
+
+
+
 }
