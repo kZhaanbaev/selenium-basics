b9536f005e45be876f704d5ad3db880d9f448c1b
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-08-20 11:53:03 -0400

    added changes in 01 and 02 classes

--- src/day03/instructor/_01_NavigateMethods.java
+++ src/day03/instructor/_01_NavigateMethods.java
@@ -1,13 +1,27 @@
 package day03.instructor;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class _01_NavigateMethods {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
         //1. Navigate methods
+        driver.navigate().to("https://youtube.com");
 
-
+        //action 2 to click on one of the videos
 
         //2. navigate() vs get()
+        driver.get("https://google.com");
 
+        //action 2 to click on one of the videos
+
+
+
+        driver.close();
 
     }
 }
--- src/day03/instructor/_02_BasicLocators.java
+++ src/day03/instructor/_02_BasicLocators.java
@@ -1,19 +1,37 @@
 package day03.instructor;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class _02_BasicLocators {
     public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://automation.techleadacademy.io/#/home");
+
         //locate by id
+        String text = driver.findElement(By.id("title")).getText();
+        //System.out.println(text);
 
         //locate by name
+        //System.out.println(driver.findElement(By.name("paragraph")).getText());
 
         //locate by className
+        //System.out.println(driver.findElement(By.className("row")).getText());
 
         //locate by tagName
+        //System.out.println(driver.findElement(By.tagName("h1")).getText());
 
         //locate by linkText
+        //System.out.println(driver.findElement(By.linkText("Internet")).isDisplayed());
 
         //locate by partialLinkText
+        System.out.println(driver.findElement(By.partialLinkText("T")).getText());
 
 
+        driver.close();
     }
 }
