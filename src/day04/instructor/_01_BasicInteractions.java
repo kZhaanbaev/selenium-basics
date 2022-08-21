80054dd355954b5eb24f228317875dbe95c412eb
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-08-21 11:53:07 -0400

    added day04

--- src/day04/instructor/_01_BasicInteractions.java
+++ src/day04/instructor/_01_BasicInteractions.java
@@ -1,10 +1,10 @@
-package day03.instructor;
+package day04.instructor;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class _03_BasicInteractions {
+public class _01_BasicInteractions {
     public static void main(String[] args) {
         /**
          * 1. Open google page
---
+++ src/day04/student/Homework2.java
@@ -0,0 +1,4 @@
+package day04.student;
+
+public class Homework2 {
+}
