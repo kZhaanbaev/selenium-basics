d0400c8227396da43477f2681c98f11ec5f6c37a
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-08-23 21:56:25 -0400

    latest changes

--- src/day05/instructor/_01_FindElementsvsFindElement.java
+++ src/day05/instructor/_01_FindElementsvsFindElement.java
@@ -1,17 +1,33 @@
 package day05.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class _01_FindElementsvsFindElement {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
+        driver.get("http://automation.techleadacademy.io/#/home");
 
         //1. What is the difference between findElement() vs findElements()
         // Nav buttons on practice home page
+        List<WebElement> paragraphs = driver.findElements(By.className("navbar-brand"));
+        System.out.println(paragraphs.size());
+
+        for(WebElement each: paragraphs){
+            System.out.println(each.getText());
+        }
+
+        //System.out.println(paragraphs.get(2).getText());
 
 
         //2.
+
+
+        driver.close();
     }
 }
