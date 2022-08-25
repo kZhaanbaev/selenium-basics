09f2ca2691995a29ff2281960f5c75381677535c
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-08-25 19:08:36 -0400

    added .md file

---
+++ src/day06/instructor/_00_Review.java
@@ -0,0 +1,13 @@
+package day06.instructor;
+
+public class _00_Review {
+    /**
+     * basic selectors
+     * css selectors:
+     * 		id
+     * 		class
+     * 		tag + id
+     * 		tag + class
+     *      tag + attribute
+     */
+}
