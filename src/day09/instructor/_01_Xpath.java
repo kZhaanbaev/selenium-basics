66d8974e9cea4e901ad60ee47e579f5e42805a9e
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-08-30 19:50:30 -0400

    class part 1 xpath

--- src/day09/instructor/_01_Xpath.java
+++ src/day09/instructor/_01_Xpath.java
@@ -10,15 +10,27 @@ public class _01_Xpath {
         driver.manage().window().maximize();
 
         //tag + attribute
+        //css -> div[class='value']
+        //xpath -> //div[@class='value']
 
         //tag + attribute + attribute
+        //css-> div[attr1='value1'][attr2='value2']
+        //xpath->   //div[@attr1='value'][@attr2='value2']
 
         //using "and" and "or" keyword when using 2 attributes
+        //xpath -> //div[@attr1='value' and @attr2='value2']
+        //xpath -> //div[@attr1='value' or @attr2='value2']
 
         //parent -> child
+        //css ->    div[class='value']>div[alt='value'] -> immediate children
+        //xpath ->  //div[@class='value']/div[@alt='value'] -> immediate children
 
         //parent -> all children
+        //css ->    div[class='value'] div[alt='value'] -> any webElement under parent hierarchy
+        //xpath ->  //div[@class='value']//div[@alt='value'] -> any webElement under parent hierarchy
 
         //asterisk *
+        //xpath -> //*[@class='value']
+
     }
 }
