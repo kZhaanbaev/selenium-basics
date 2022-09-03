ea77d50780ad2b150aa8d2e1890f348749443c85
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-03 09:35:29 -0400

    part 1 done

--- src/day11/instructor/_01_SelectClass.java
+++ src/day11/instructor/_01_SelectClass.java
@@ -1,26 +1,41 @@
 package day11.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 public class _01_SelectClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("http://automation.techleadacademy.io/#/selectclass");
 
         //when to use Select class?
+        Select select = new Select(driver.findElement(By.name("select1")));
 
         //select by text
+        select.selectByVisibleText("Three");
 
         //select by index
+        select.selectByIndex(6);
 
         //select by value
+        select.selectByValue("Nine");
 
         //how to get all selectable options?
+        List<WebElement> allOptions = select.getOptions();
+        allOptions.forEach(each -> System.out.println(each.getText()));
 
         //ClassTask 1: 10min
 
+
+        Thread.sleep(3000);
+        driver.close();
 
     }
 }
