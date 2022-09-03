a394ad9ad20591cf9d61a6a6195ee8ed7c6b409f
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-03 12:10:47 -0400

    updated homework details

--- src/day11/student/_04_ClassTask.java
+++ src/day11/student/_04_ClassTask.java
@@ -4,6 +4,8 @@ public class _04_ClassTask {
     /**
      * Alerts
      * Password Generator section:
-     * Go through steps to choose type of password to generate and print out the result
+     * Go through steps to choose type of password to generate and print out:
+     * 1. Last alert window
+     * 2. The result from the generated password window
      */
 }
