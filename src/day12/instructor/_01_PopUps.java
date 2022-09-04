dd08a7db242d2fe3fd5287b59d78ca812f67335f
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-04 10:59:18 -0400

    demo

--- src/day12/instructor/_01_PopUps.java
+++ src/day12/instructor/_01_PopUps.java
@@ -1,5 +1,6 @@
 package day12.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -8,11 +9,20 @@ public class _01_PopUps {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.get("http://automation.techleadacademy.io/#/popup");
 
         //What is browser pop-ups? Pop-ups vs alerts?
 
         //Example: Pop-Up -> Message
+        driver.findElement(By.tagName("button")).click();
+        driver.findElement(By.id("exampleInputEmail1")).sendKeys("This is my message");
+
+
+
 
         //Class Task 1
     }
+
+
+
 }
