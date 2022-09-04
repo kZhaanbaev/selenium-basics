bb2adc6af9e4df8916f7b0fe23780554a9f3d411
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-03 22:57:34 -0400

    blank day 12

--- src/day12/instructor/_01_PopUps.java
+++ src/day12/instructor/_01_PopUps.java
@@ -1,13 +1,14 @@
-package day11.instructor;
+package day12.instructor;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class _03_PopUps {
+public class _01_PopUps {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+
 
         
     }
---
+++ src/day12/student/_01_ClassTask.java
@@ -0,0 +1,4 @@
+package day12.student;
+
+public class _01_ClassTask {
+}
