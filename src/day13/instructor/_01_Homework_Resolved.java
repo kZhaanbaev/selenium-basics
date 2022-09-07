e7dc94f7f8626f527095edd46072431f7b5f1e74
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-06 20:56:23 -0400

    done in class

--- src/day13/instructor/_01_Homework_Resolved.java
+++ src/day13/instructor/_01_Homework_Resolved.java
@@ -1,7 +1,12 @@
 package day13.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
 
 public class _01_Homework_Resolved {
     /**
@@ -16,9 +21,61 @@ public class _01_Homework_Resolved {
      *  9. Click on "Launch TLA"
      *  10. Print out text of navigation buttons
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+
+        driver.get("http://automation.techleadacademy.io/#/multiplewindow");
+        List<WebElement> btns = driver.findElements(By.xpath("//a[starts-with(@class, 'btn')]"));
+        btns.forEach(button -> System.out.println(button.getText()));
+
+        btns.get(2).click();
+
+        String mainWindowID = driver.getWindowHandle();
+        Set<String> allWindows = driver.getWindowHandles();
+
+        for(String each: allWindows){
+            if(!each.equals(mainWindowID))
+                driver.switchTo().window(each);
+        }
+
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+        driver.close();
+        
+        driver.switchTo().window(mainWindowID);
+
+        //clicking on google button
+        btns.get(1).click();
+
+        allWindows = driver.getWindowHandles();
+
+        for(String each: allWindows){
+            if(!each.equals(mainWindowID))
+                driver.switchTo().window(each);
+        }
+
+        System.out.println(driver.getTitle());
+
+        driver.switchTo().window(mainWindowID);
+
+        btns.get(0).click();
+
+        allWindows = driver.getWindowHandles();
+
+        for(String each: allWindows){
+            driver.switchTo().window(each);
+            if(driver.getTitle().contains("Tech Lead Academy")){
+                driver.manage().window().maximize();
+                List<WebElement> navBtns = driver.findElements(By
+                        .xpath("//div[@id='lp-pom-box-346']/div[contains(@class,'nlh')]"));
+                navBtns.forEach(eachBtn -> System.out.println(eachBtn.getText()));
+                break;
+            }
+        }
+
+
+        Thread.sleep(3000);
+        driver.quit();
     }
 }
--- src/day13/instructor/_02_Tables.java
+++ src/day13/instructor/_02_Tables.java
@@ -1,7 +1,11 @@
 package day13.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
 
 public class _02_Tables {
     public static void main(String[] args) {
@@ -9,15 +13,40 @@ public class _02_Tables {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
 
+        driver.get("http://automation.techleadacademy.io/#/tables");
+
         //Print out all emails
+        List<WebElement> emails = driver.findElements(By.xpath("//tr/td[6]"));
+        //emails.forEach(a -> System.out.println(a.getText()));
 
         //Print out all first names
+        List<WebElement> names = driver.findElements(By.xpath("//tr/td[1]"));
+        //names.forEach(a -> System.out.println(a.getText()));
 
         //Print out all first names with emails
+        for(int i = 0; i < names.size(); i++){
+            //System.out.println(names.get(i).getText() + " : " + emails.get(i).getText());
+        }
 
         //Print out all first names with emails but as groups
+        List<WebElement> customers = driver.findElements(By.xpath("//tbody/tr"));
+
+        for(WebElement eachCustomer: customers){
+//            System.out.println(eachCustomer.findElement(By.xpath("./td[1]")).getText()
+//            + " : " + eachCustomer.findElement(By.xpath("./td[6]")).getText());
+        }
+
 
         //Print out value of First names based on if gender is Male (in Tables page)
+        for(WebElement eachCustomer: customers){
+            if(eachCustomer.findElement(By.xpath("//td[3]")).getText().equals("Male"))
+                System.out.println(
+                        eachCustomer.findElement(By.xpath("./td[1]")).getText()
+                );
+        }
+
+        driver.close();
+
 
         //Class Task 1
     }
