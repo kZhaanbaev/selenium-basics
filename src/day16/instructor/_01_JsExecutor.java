b3814b93d1e866a71d4bb55d6559559db0179bac
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-11 10:49:44 -0400

    01 done

--- src/day16/instructor/_01_JsExecutor.java
+++ src/day16/instructor/_01_JsExecutor.java
@@ -1,18 +1,53 @@
 package day16.instructor;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class _01_JsExecutor {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://automation.techleadacademy.io/#/files");
+        driver.get("http://automation.techleadacademy.io/#/home");
 
-        //implicit wait
+        //Scroll into view
+        WebElement element = driver.findElement(By.linkText("Shopping Cart"));
+        moveIntoView(element, driver);
 
+        highlightElement(element, driver);
 
-        //explicit wait (synchronization page - Display message)
+    }
+
+    public static void moveIntoView(WebElement element, WebDriver driver){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        sleep(2000L);
+
+        js.executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    public static void highlightElement(WebElement element, WebDriver driver){
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+
+        sleep(2000L);
+
+        js.executeScript("arguments[0].setAttribute('style','border: solid 2px red');", element);
+        js.executeScript("arguments[0].style.backgroundColor='yellow'", element);
+
+        sleep(2000L);
+
+        js.executeScript("arguments[0].setAttribute('style','border: none');", element);
+        js.executeScript("arguments[0].style.backgroundColor=null", element);
+    }
+
+    public static void sleep(Long milliSeconds){
+        try {
+            Thread.sleep(milliSeconds);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
