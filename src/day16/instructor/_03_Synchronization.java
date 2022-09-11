3e9ea69871695ee01c2efb056ee743feea77e3fb
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-11 10:17:05 -0400

    new changes day16

---
+++ src/day16/instructor/_03_Synchronization.java
@@ -0,0 +1,17 @@
+package day16.instructor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _03_Synchronization {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://automation.techleadacademy.io/#/files");
+
+        //implicitly wait
+
+        //explicit waits
+    }
+}
---
+++ src/day16/instructor/_04_Synchronization.java
@@ -0,0 +1,15 @@
+package day16.instructor;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class _04_Synchronization {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://automation.techleadacademy.io/#/files");
+
+        //fluent wait
+    }
+}
