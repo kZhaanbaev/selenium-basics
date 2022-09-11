fb94ef64fd85929117c353421b6c93be47e43160
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-11 11:47:55 -0400

    02 and 03 done

--- src/day16/instructor/_02_HeadlessBrowser.java
+++ src/day16/instructor/_02_HeadlessBrowser.java
@@ -1,15 +1,26 @@
 package day16.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class _02_HeadlessBrowser {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
-        WebDriver driver = new ChromeDriver();
+
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("http://automation.techleadacademy.io/#/files");
+        driver.get("http://automation.techleadacademy.io");
 
+        System.out.println(driver.findElement(By.id("title")).getText());
 
+        driver.findElement(By.linkText("User-Mgt")).click();
+        System.out.println(driver.findElement(By.tagName("h2")).getText());
+
+        driver.close();
     }
 }
--- src/day16/instructor/_03_Synchronization.java
+++ src/day16/instructor/_03_Synchronization.java
@@ -1,17 +1,33 @@
 package day16.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class _03_Synchronization {
     public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.get("http://automation.techleadacademy.io/#/files");
+        //implicit wait goes here
+        driver.get("http://automation.techleadacademy.io/#/synchronization");
 
         //implicitly wait
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
         //explicit waits
+        driver.findElement(By.id("input-text")).sendKeys("SDET");
+        driver.findElement(By.tagName("button")).click();
+
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[text()='SDET']")));
+
+        System.out.println(driver.findElement(By.xpath("//*[text()='SDET']")).isDisplayed());
+
+        driver.close();
     }
 }
