f80e458dd8e79e1901a9802f440b0cbc8d45d77a
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-17 10:02:42 -0400

    demo1 complete

--- test/day19/instructor/_01_TDD.java
+++ test/day19/instructor/_01_TDD.java
@@ -2,6 +2,7 @@ package day19.instructor;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -9,17 +10,51 @@ import java.util.concurrent.TimeUnit;
 import static org.testng.Assert.assertEquals;
 
 public class _01_TDD {
+    //Test Driven Development
+
+    public int getCountOfChar(String txt, String inputChar){
+        String[] arr = txt.split("");
+
+        int count = 0;
+
+        for(String each: arr){
+            if (each.equalsIgnoreCase(inputChar))
+                count++;
+        }
+
+        return count;
+    }
+
+    /**
+     * write a test case(s) first and then create functionality
+     */
 
     @Test
     public void test1(){
-        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        _01_TDD obj = new _01_TDD();
 
-        driver.get("https://google.com");
-        assertEquals(driver.getTitle(), "Google");
+        String testData = "Tech Lead Academy";
+        String inputChar = "a";
+        int expectedCount = 3;
+        int actualCount = obj.getCountOfChar(testData, inputChar);
 
-        driver.close();
+        Assert.assertEquals(actualCount, expectedCount);
     }
+
+
+
+
+
+//    @Test
+//    public void test1(){
+//        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+//        WebDriver driver = new ChromeDriver();
+//        driver.manage().window().maximize();
+//        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//
+//        driver.get("https://google.com");
+//        assertEquals(driver.getTitle(), "Google");
+//
+//        driver.close();
+//    }
 }
--- test/day19/instructor/_02_TestNGAnnotations.java
+++ test/day19/instructor/_02_TestNGAnnotations.java
@@ -1,14 +1,16 @@
 package day19.instructor;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-import static org.testng.Assert.assertEquals;
-
 public class _02_TestNGAnnotations {
-    public static void main(String[] args) {
+    /**
         System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
@@ -20,10 +22,68 @@ public class _02_TestNGAnnotations {
 
         //3. "description" - Verify "Enter message" section on Inputs page displaying expected txt
 
-        //4. "enable/disable" - Verify Notes page header "Curriculum" displayed
+        //4. "enable/disable" - Verify Curriculum page header "Curriculum"
 
         //ClassTask 1
+    */
+
+    @Test
+    public void testGoogle(){
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("https://google.com");
+
+        Assert.assertEquals(driver.getTitle(), "Google");
+        driver.close();
     }
+
+    @Test(testName = "Test Header")
+    public void test02(){
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://automation.techleadacademy.io/#/home");
+
+        String actualHeader = driver.findElement(By.id("title")).getText();
+        String expectedHeader = "Automation with Selenium";
+
+        Assert.assertEquals(actualHeader, expectedHeader);
+        driver.close();
+    }
+
+    @Test(testName = "Test Input", description = "Testing if input data is displayed as expected")
+    public void test03(){
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://automation.techleadacademy.io/#/inputs");
+
+        String testData = "Blue Sky";
+        driver.findElement(By.id("message")).sendKeys(testData);
+        driver.findElement(By.name("button1")).click();
+
+        WebElement element = driver.findElement(By.xpath("//span[@name='message1' and text()='" + testData + "']"));
+        Assert.assertTrue(element.isDisplayed());
+
+        driver.close();
+    }
+
+    @Test(testName = "Curriculum Page Test", description = "Testing main header of the page", enabled = false)
+    public void test04(){
+        System.setProperty("webdriver.chrome.driver", "/Users/kuba/Desktop/Selenium/libs/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.get("http://automation.techleadacademy.io/#/curriculum");
+
+        Assert.assertTrue(driver.findElement(By.xpath("//h1[text()='Curriculum']")).isDisplayed());
+        driver.close();
+    }
+
 
 
 }
