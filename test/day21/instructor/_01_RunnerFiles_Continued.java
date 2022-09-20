154a22c0da468068cf38666b34fde0a4113fb244
Kubanychbek Zhaanbaev <k.zhaanbaev4@gmail.com>
2022-09-20 19:06:07 -0400

    day 21

---
+++ test/day21/instructor/_01_RunnerFiles_Continued.java
@@ -0,0 +1,22 @@
+package day21.instructor;
+
+public class _01_RunnerFiles_Continued {
+    /**
+     *  1. README file - xml section - DONE
+     *  2. Create runner files: (students in "student" folder along with instructor)
+     *      a. Running multiple packages - DONE
+     *      b. Running multiple classes - DONE
+     *      c. Running specific Test methods - DONE
+     *
+     *
+     *      Review above
+     *
+     *      d. Grouping Test cases
+     *      e. Parameters
+     *      d. Annotations at Class level
+     *
+     *  3. Reports
+     *  4. Run failed Test cases
+     *
+     */
+}
---
+++ test/day21/student/_01_US1010.java
@@ -0,0 +1,11 @@
+package day21.student;
+
+/**
+ * US1010 - Staging table view
+ * 	I need an option to add new users to temporary table. Adding users to that table should not add them to the database
+ */
+public class _01_US1010 {
+
+
+
+}
